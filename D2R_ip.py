848effacfc9490ccef1832ccc78ba9b4e91c9743
Nocawy <nocawy@users.noreply.github.com>
2021-11-16 21:43:56 +0100

    take hunted IP as parameter; display subregion

--- D2R_ip.py
+++ D2R_ip.py
@@ -1,49 +1,55 @@
+import sys
 import psutil
 import logging
 from time import strftime, time, sleep
 from datetime import datetime
 from colorama import init, Fore, Style
 from sched import scheduler
+from math import floor
 
 update_interval = 1     # refresh rate in seconds; setting lower than 1 might be pointless because the process of checking connections takes close to 1s
 logfile = 'D2R_ip_{}.log'.format(strftime('%Y-%m-%d'))
+datetime_format = '%Y-%m-%d %H:%M:%S'
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s, %(message)s', datefmt='%Y-%m-%d, %H:%M:%S',
+    format='%(asctime)s, %(message)s', datefmt=datetime_format,
     handlers=[
         logging.FileHandler(logfile),   # remove this line to stop logging to a file
         logging.StreamHandler()
     ]
 )
 
-global_client = [
-    '24.105.29.76',     # client
-    '34.117.122.6',     # client
+constant_ips = [
+    '24.105.29.76',     # always open
+    '34.117.122.6',     # always open
     '127.0.0.1',        # localhost
+    '137.221.105.152',  # Americas lobby
+    '37.244.54.10',     # Europe lobby
+    '117.52.35.45',     # Asia lobby
 ]
-EU_client = [
-    '37.244.28.80',     # EU client
-    '37.244.28.180',    # EU client
-    '37.244.54.10',     # EU client
-]
-NA_client = [
-    '137.221.106.88',   # NA client
-    '137.221.106.188',  # NA client
-    '137.221.105.152',  # NA client
-]
-Asia_client = [
-    '117.52.35.45',     # Asia client
-    '117.52.35.79',     # Asia client
-    '117.52.35.179',    # Asia client
-]
+# regions = [
+#     '137.221.106.88',   # Americas
+#     '137.221.106.188',  # Americas
+#     '37.244.28.80',     # Europe
+#     '37.244.28.180',    # Europe
+#     '117.52.35.79',     # Asia
+#     '117.52.35.179',    # Asia
+# ]
 
 init()  # colorama initialisation
 s = scheduler(time, sleep)
 
 previous_ip = 0
 current_game_ip = 0
-previous_time = 0
+previous_time = time()
+hunting_ip=''
+
+print("D2R IP Logger started. To exit press CTRL+C")
+if len(sys.argv) >= 2:
+    print('Hunting for ip: ' + Fore.LIGHTRED_EX + sys.argv[1] + Style.RESET_ALL)
+    hunting_ip = sys.argv[1]
+
 def print_ip():
     s.enter(update_interval, 1, print_ip)   # run this function every update_interval
     d2r_pid = 0
@@ -61,30 +67,42 @@ def print_ip():
     global current_game_ip
     global previous_time
     region = '?'
+    subregion = '?'
     for c in p.connections('tcp'):
         if (c.raddr):
             ip = c.raddr.ip
-            if(ip in EU_client):
-                region = 'EU'
-            elif(ip in NA_client):
-                region = 'NA'
-            elif(ip in Asia_client):
+            if(ip == '37.244.28.80'):
+                region = 'Europe'
+                subregion = '80'
+            elif(ip == '37.244.28.180'):
+                region = 'Europe'
+                subregion = '180'
+            elif(ip == '137.221.106.88'):
+                region = 'Americas'
+                subregion = '88'
+            elif(ip == '137.221.106.188'):
+                region = 'Americas'
+                subregion = '188'
+            elif(ip == '117.52.35.79'):
                 region = 'Asia'
-            elif(ip not in global_client):
+                subregion = '79'
+            elif(ip == '117.52.35.179'):
+                region = 'Asia'
+                subregion = '179'
+            elif(ip not in constant_ips):
                 current_game_ip = ip
     if(current_game_ip!=previous_ip):
         # found a new game ip, log it
         print(' '*44, end="\r", flush=True) #clear line
-        logging.info('{}, {}'.format(region, current_game_ip))
+        if current_game_ip == hunting_ip:
+            print(Fore.LIGHTRED_EX, end="\r", flush=True)
+        logging.info('{},{}, {}'.format(region, subregion, current_game_ip))
+        if current_game_ip == hunting_ip:
+            print(Style.RESET_ALL, end="\r", flush=True)
         previous_ip = current_game_ip
         previous_time = time()
-    # display clock, coloured green if more then 1 minute passed since last game creation:
-    msg = datetime.now().strftime('%Y-%m-%d, %H:%M:%S')
-    info = ' (press CTRL+C to exit)'
-    if previous_time>0 and time()-previous_time>=60:
-        print(Fore.GREEN + msg + Style.RESET_ALL + info, end="\r", flush=True)
-    else:
-        print(msg + info, end="\r", flush=True)
+    # display clock and seconds passed since last entry
+    print(datetime.now().strftime(datetime_format)+", "+str(floor(time()-previous_time)), end="\r", flush=True)
 
 s.enter(0, 1, print_ip)
 try:
